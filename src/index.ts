039a3a245fade00c81665cf800d7cc6459ff4151
rafia <akmalrafi1132@gmail.com>
2026-01-27 09:13:22 +0700

    feat: test prisma

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,23 @@
 import express from "express";
+import { PrismaClient } from "@prisma/client";
 
 const app = express();
+const prisma = new PrismaClient();
 
-app.get("/", (_req, res) => {
-  res.send("OK EXPRESS TS JALAN 🚀");
+app.get("/", async (_req, res) => {
+  // test prisma hidup
+  const result = await prisma.$queryRaw`SELECT 1`;
+  res.json({
+    status: "ok",
+    prisma: "connected",
+    result,
+  });
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log("running on " + PORT);
+// ⚠️ penting: ini boleh ADA di Vercel
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`🚀 running on http://localhost:${port}`);
 });
+
+export default app;
